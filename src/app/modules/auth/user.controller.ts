a39bef4ab8ccdbee27f4a4d01852086e46af5d3b
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2026-03-08 00:58:48 +0600

    done

--- src/app/modules/auth/user.controller.ts
+++ src/app/modules/auth/user.controller.ts
@@ -47,7 +47,12 @@ const verifyEmailController = catchAsync(async (req: Request, res: Response) => 
 
   // verifyOtp service → OTP check + DB save
   const user = await authServices.verifyEmail(email, Number(otp));
- const accessToken = jwt.sign({ id: user._id, role: user.role }, config.jwt.jwt_access_secret as string, { expiresIn: '24h' });
+  const accessToken = jwt.sign(
+    { id: user._id, role: user.role },
+    config.jwt.jwt_access_secret as string,
+    { expiresIn: '24h' }
+  );
+
   sendResponse(res, {
     statusCode: httpStatus.CREATED,
     success: true,
